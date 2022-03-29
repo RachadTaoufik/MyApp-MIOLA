17ff04acb2885f4a166a566d166ba438c40d038e
Taoufik Rachad <rachad.taoufik@gmail.com>
2022-03-29 10:43:13 +0000

    ok

--- app/src/main/java/com/example/myapp/LoginActivity.java
+++ app/src/main/java/com/example/myapp/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -56,6 +58,21 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             linearLayout0.setBackgroundColor(Color.GREEN);
         else if(item.getItemId()==R.id.item_yellow)
             linearLayout0.setBackgroundColor(Color.YELLOW);
+        else if(item.getItemId()==R.id.item_About){
+            Intent myIntent= new Intent(this, aboutActivity.class);
+            startActivity(myIntent);}
+
+        else if(item.getItemId()==R.id.item_Help){
+            Intent email = new Intent();
+            email.setData(Uri.parse("mailto:"));
+            email.setAction(Intent.ACTION_SEND);
+            email.putExtra(Intent.EXTRA_EMAIL, "rachad.taoufik@gmail.com");
+            email.putExtra(Intent.EXTRA_SUBJECT, "Help");
+            email.putExtra(Intent.EXTRA_TEXT, "Veuillez m'envoyer mes informations d'authentification");
+            startActivity(Intent.createChooser(email,"Veuillez choisir une application..."));
+
+
+        }
 
         return true;
     }
