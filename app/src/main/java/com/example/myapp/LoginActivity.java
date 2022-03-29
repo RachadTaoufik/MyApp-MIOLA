9c794a643dd2b2be3905ccf9587b5d5ba59c3cf2
Taoufik Rachad <rachad.taoufik@gmail.com>
2022-03-29 10:08:56 +0000

    ok

--- app/src/main/java/com/example/myapp/LoginActivity.java
+++ app/src/main/java/com/example/myapp/LoginActivity.java
@@ -2,7 +2,11 @@ package com.example.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +17,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     Button button_login;
     Button button_logout;
     Button button_singup;
+    LinearLayout    linearLayout0;
     LinearLayout    linearLayout1;
     LinearLayout    linearLayout2;
     LinearLayout    linearLayout3;
@@ -30,10 +35,29 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         button_login=findViewById(R.id.BTT_login);
         //...
         button_login.setOnClickListener(this);
+        linearLayout0=findViewById(R.id.Layout_0);
     }
     @Override
     public void onClick(View view) {
 
 
     }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_1,menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        if (item.getItemId()==R.id.item_red)
+            linearLayout0.setBackgroundColor(Color.RED);
+        else if(item.getItemId()==R.id.item_green)
+            linearLayout0.setBackgroundColor(Color.GREEN);
+        else if(item.getItemId()==R.id.item_yellow)
+            linearLayout0.setBackgroundColor(Color.YELLOW);
+
+        return true;
+    }
+
 }
