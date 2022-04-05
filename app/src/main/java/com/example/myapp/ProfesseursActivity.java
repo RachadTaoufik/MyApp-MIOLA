18b8c7ecc3e103c6ecaf386ce209cdab787a2ee9
Taoufik Rachad <rachad.taoufik@gmail.com>
2022-04-05 14:35:22 +0000

    ok

--- app/src/main/java/com/example/myapp/ProfesseursActivity.java
+++ app/src/main/java/com/example/myapp/ProfesseursActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ProfesseursActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+public class ProfesseursActivity extends AppCompatActivity {
+    FloatingActionButton FabAdd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_professeurs);
+
+        FabAdd.findViewById(R.id.fabAdd);
     }
 }
