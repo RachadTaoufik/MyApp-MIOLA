7ea87b9c15623a2a188b20353b2a73c267032706
Taoufik Rachad <rachad.taoufik@gmail.com>
2022-04-12 11:47:05 +0000

    ok

--- app/src/main/java/com/example/myapp/ProfesseursActivity.java
+++ app/src/main/java/com/example/myapp/ProfesseursActivity.java
@@ -2,13 +2,12 @@ package com.example.myapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-import com.example.myapp.adapter.ProfesseurAdapter;
 import com.example.myapp.model.Professeur;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -17,46 +16,39 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 
-public class ProfesseursActivity extends AppCompatActivity {
+public class ProfesseursActivity extends AppCompatActivity{
     FloatingActionButton FabAdd;
     FirebaseFirestore db;
     LinkedList<Professeur> profs;
-    RecyclerView.LayoutManager layoutManager;
+    ListView mylist;
+    public ProgressDialog mProgressDialog;
 
-    RecyclerView myrecycler;
             ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_professeurs);
-
         FabAdd=findViewById(R.id.fabAdd);
         profs= new LinkedList<Professeur>();
-
-
+        mylist=findViewById(R.id.list_profs);
         db = FirebaseFirestore.getInstance();
-        getAllProfesseurs();
-
 
 
     }
 
-    protected void onStart(){
-        super.onStart();
-        layoutManager = new LinearLayoutManager(this);
-        myrecycler=findViewById(R.id.myRecycler);
+    protected void onResume(){
+        super.onResume();
         getAllProfesseurs();
-        ProfesseurAdapter PA= new ProfesseurAdapter(profs,this);
-        myrecycler.setLayoutManager(layoutManager);
-        myrecycler.setAdapter(PA);
+
     }
 
 
 
     void getAllProfesseurs(){
-        LinkedList<Professeur> profs= new LinkedList<Professeur>();
+        showProgressDialog();
         db.collection("professeur")
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
@@ -73,12 +65,33 @@ public class ProfesseursActivity extends AppCompatActivity {
                         }
                         else
                             System.out.println("Erreur!!!!!");
+
+                        ArrayList<String> noms_prof = new ArrayList<String>();
+                        for(Professeur prof: profs){
+                            noms_prof.add(prof.getNom());
+                        }
+                        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(ProfesseursActivity.this,android.R.layout.simple_list_item_1,noms_prof );
+                        mylist.setAdapter(arrayAdapter);
+                        hideProgressDialog();
                     }
-
-                    // intialiser la liste
-
                 });
         System.out.println("La liste des profs :"+profs);
+    }
+
+    public void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
     }
 
 }
