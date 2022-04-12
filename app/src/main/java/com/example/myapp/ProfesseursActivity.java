e309a103ce2843c4562df912771bcf7ca1ce2842
Taoufik Rachad <rachad.taoufik@gmail.com>
2022-04-12 12:12:02 +0000

    ok

--- app/src/main/java/com/example/myapp/ProfesseursActivity.java
+++ app/src/main/java/com/example/myapp/ProfesseursActivity.java
@@ -5,8 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.myapp.model.Professeur;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -92,6 +96,50 @@ public class ProfesseursActivity extends AppCompatActivity{
         if (mProgressDialog != null && mProgressDialog.isShowing()) {
             mProgressDialog.dismiss();
         }
+
+
+
+
+
     }
+
+    private class MyAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return profs.size();
+        }
+
+        @Override
+        public Professeur getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            if (view == null) {
+                view = getLayoutInflater().inflate(R.layout.list_item, viewGroup,false);
+            }
+
+            ((TextView) view.findViewById(R.id.prof_textView))
+                    .setText(getItem(i).getPrenom()+" "+getItem(i).getNom());
+
+            // Other components
+            // for image use Glide
+
+
+           return view;
+        }
+
+        // override other abstract methods here
+
+    }
+
 
 }
